843f23e37e7de5a213f2e3ef0831e1542545b4ea
AnveshCS21 <anveshCS21-G4@coastalseven.com>
2025-12-24 12:41:10 +0530

    Update database.py

--- JSONB_API/database.py
+++ JSONB_API/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:root@localhost/jsonb_db"
+DATABASE_URL = "postgresql://postgres:your_password@localhost/jsonb_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
